4ffe9796dcf6d5192fbb08bd180d1e0dd006c2b2
Filippov Ivan <fim1902@gmail.com>
2023-07-29 17:56:31 +0300

    multicore optimization for distance computation

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from utils.constants import DatasetType, DAY_IN_SEC
 from utils.data_reading import clean_and_store_data, read_cleaned_data, \
     convert_to_nilmtk_format, store_processed_stable_periods, \
     get_full_keys_of_stable_periods, read_stable_periods
+from utils.informative_index import accumulated_distance
 from utils.preprocessing import generate_sync_signals, get_stable_periods, \
     interpolate_missed_data, generate_async_signals, reformat_to_accumulated
 from utils.timing import time_measure
@@ -22,13 +23,15 @@ MAX_GAP = 300
 # ideal = read_cleaned_data(DS)
 # process_stable_periods(DS, DURATION, MAX_GAP)
 
-full_keys = get_full_keys_of_stable_periods(DS)
-accumulated = read_stable_periods(DS, [full_keys[0]])[0]
+if __name__ == '__main__':
+    full_keys = get_full_keys_of_stable_periods(DS)
+    accumulated = read_stable_periods(DS, [full_keys[0]])[0]
 
-sync_signals = generate_sync_signals(accumulated, 100)
-print('len(sync_signals)', len(sync_signals))
+    sync_signals = generate_sync_signals(accumulated, 100)
+    print('len(sync_signals)', len(sync_signals))
 
-async_signals = generate_async_signals(accumulated, 60000)
-print('len(async_signals)', len(async_signals))
+    async_signals = generate_async_signals(accumulated, 60000)
+    print('len(async_signals)', len(async_signals))
 
-# plot_sync_async_comparison(accumulated, sync_signals, async_signals)
+    # plot_sync_async_comparison(accumulated, sync_signals, async_signals)
+    accumulated_distance(accumulated, async_signals, 4)
--- utils/informative_index.py
+++ utils/informative_index.py
@@ -2,12 +2,29 @@ import numpy as np
 import pandas as pd
 from numpy.polynomial.polynomial import Polynomial
 from scipy.interpolate import InterpolatedUnivariateSpline, splrep, PPoly
+from multiprocessing.pool import ThreadPool, Pool
 
 from utils.preprocessing import datetime_index_to_floats
 from utils.timing import time_measure
 
 
-def accumulated_distance(original: pd.Series, generated: pd.Series):
+def calc_squared_coefs(interpolations, i):
+    p = Polynomial(interpolations.c[:, i][::-1]) / 100
+    p_squared = p ** 2
+    squared_coefs = p_squared.coef[::-1]
+    squared_coefs = np.pad(squared_coefs, (0, 3 - len(squared_coefs)))
+    return squared_coefs
+
+
+def calc_squared_coefs_range(interpolations, start, end):
+    with time_measure(f'calc_squared_coefs_range from {start} to {end}', is_active=False):
+        return [
+            calc_squared_coefs(interpolations, i)
+            for i in range(start, end)
+        ]
+
+
+def accumulated_distance(original: pd.Series, generated: pd.Series, parallel: int = 1):
     with time_measure(f'accumulated_distance'):
         f_dates_orig = datetime_index_to_floats(original.index)
         f_dates_gen = datetime_index_to_floats(generated.index)
@@ -26,20 +43,23 @@ def accumulated_distance(original: pd.Series, generated: pd.Series):
         assert interpolations.c.shape[0] == 2, 'must be linear'
         assert interpolations.c.shape[1] == point_count + 1, 'interval count is wrong'
 
-        squared_coefs = np.empty((3, point_count + 1), dtype=np.float64)
-        for i in range(point_count + 1):
-            p = Polynomial(interpolations.c[:, i][::-1]) / 100
-            p_squared = p ** 2
-            coefs_to_apply = p_squared.coef[::-1]
-            coefs_to_apply = np.pad(coefs_to_apply, (0, 3 - len(coefs_to_apply)))
-            for ind, c in enumerate(coefs_to_apply):
-                squared_coefs[ind, i] = c
+        if parallel == 1:
+            squared_coefs = calc_squared_coefs_range(interpolations, 0, point_count + 1)
+            squared_coefs = np.stack(squared_coefs, axis=0)
+        else:
+            chunk_size = point_count // parallel + 1
+            arguments = [
+                (interpolations, i, min(i+chunk_size, point_count+1))
+                for i in range(0, point_count + 1, chunk_size)
+            ]
+            with Pool(parallel) as pool:
+                squared_coefs_parts = pool.starmap(calc_squared_coefs_range, arguments)
+            squared_coefs = np.concatenate(squared_coefs_parts, axis=0)
 
-        squared_diffs = PPoly(squared_coefs, interpolations.x)
+        squared_diffs = PPoly(squared_coefs.T, interpolations.x)
 
         distance = squared_diffs.integrate(united_dates[0], united_dates[-1])
-        print('distance')
-        print(distance)
+        print('distance', distance)
         return distance
 
 
