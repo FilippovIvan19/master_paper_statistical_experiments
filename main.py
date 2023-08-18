212f2e0247bb3de16673aa3eaa83cd3a7239fb78
Filippov Ivan <fim1902@gmail.com>
2023-08-18 22:47:54 +0300

    make main to be convenient for terminal usage

--- main.py
+++ main.py
@@ -1,26 +1,30 @@
+import argparse
+
 import numpy as np
 from pandas import HDFStore
 
 from nilmtk import STATS_CACHE
 
-from utils.constants import DatasetType, DAY_IN_SEC, DURATION_MAX_GAP_PAIRS
-from utils.data_reading import clean_and_store_data, read_cleaned_data, \
-    convert_to_nilmtk_format, store_processed_stable_periods, \
-    get_full_keys_of_stable_periods, read_stable_periods, process_stable_periods
-from utils.experiment import run_experiment_accum
-from utils.informative_index import accumulated_distance, power_distance
-from utils.preprocessing import generate_sync_signals, get_stable_periods, \
-    interpolate_missed_data, generate_async_signals, reformat_to_accumulated, \
-    generate_sync_signals_by_point_count, generate_async_signals_by_point_count
+from utils.constants import DatasetType, DAY_IN_SEC, DURATION_MAX_GAP_PAIRS, TIME_DELTAS
+# from utils.data_reading import process_stable_periods
+# from utils.data_reading import clean_and_store_data, read_cleaned_data, \
+#     convert_to_nilmtk_format, store_processed_stable_periods, \
+#     get_full_keys_of_stable_periods, read_stable_periods, process_stable_periods, \
+#     build_full_key, parse_full_key
+from utils.experiment import run_experiment_accum, run_experiment_power
+# from utils.informative_index import accumulated_distance, power_distance
+# from utils.preprocessing import generate_sync_signals, get_stable_periods, \
+#     interpolate_missed_data, generate_async_signals, reformat_to_accumulated, \
+#     generate_sync_signals_by_point_count, generate_async_signals_by_point_count
 from utils.timing import time_measure
-from utils.visualization import plot_sync_async_comparison
+# from utils.visualization import plot_sync_async_comparison
 
 STATS_CACHE.store.close()
 
 
-DS = DatasetType.REDD
-DURATION = DAY_IN_SEC*30
-MAX_GAP = 300
+DS = DatasetType.IDEAL
+# DURATION = DAY_IN_SEC*30
+# MAX_GAP = 300
 
 # convert_to_nilmtk_format(DS)
 # clean_and_store_data(DS)
@@ -28,8 +32,37 @@ MAX_GAP = 300
 # process_stable_periods(DS, DURATION, MAX_GAP)
 
 if __name__ == '__main__':
-    with time_measure(f'run_experiment_accum'):
-        run_experiment_accum(DatasetType.REDD, 60, "output.xlsx", slice(-6, -4), 6)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('experiment', choices=['accum', 'power'])
+    parser.add_argument('dataset', choices=[elem.path_str for elem in DatasetType])
+    parser.add_argument('time_delta_idx', type=int, choices=range(len(TIME_DELTAS)))
+    parser.add_argument('dur_gap_start', type=int, choices=range(len(DURATION_MAX_GAP_PAIRS)))
+    parser.add_argument('dur_gap_end', type=int, choices=range(1, len(DURATION_MAX_GAP_PAIRS)+1))
+    parser.add_argument('processes', type=int, nargs='?', default=1)
+    args = parser.parse_args()
+
+    if args.experiment == 'accum':
+        experiment_func = run_experiment_accum
+        experiment_name = 'run_experiment_accum'
+    else:
+        experiment_func = run_experiment_power
+        experiment_name = 'run_experiment_power'
+
+    with time_measure(experiment_name):
+        experiment_func(
+            DatasetType.get_by_name(args.dataset),
+            TIME_DELTAS[args.time_delta_idx],
+            f'{args.experiment}_{args.dataset}_td{TIME_DELTAS[args.time_delta_idx]}_dur_gap{args.dur_gap_start}-{args.dur_gap_end}.xlsx',
+            slice(args.dur_gap_start, args.dur_gap_end),
+            args.processes
+        )
+
+
+
+    # print(DatasetType.REDD)
+    # print(DatasetType.get_by_name('redd'))
+    # print(DatasetType.get_by_name('ideal'))
+    # print(DatasetType.get_by_name('hjkdfshjshjk'))
 
 
     # dur, gap = DURATION_MAX_GAP_PAIRS[-1]
@@ -37,11 +70,13 @@ if __name__ == '__main__':
     # process_stable_periods(DS, dur, gap)
 
 
-    # print(len(get_full_keys_of_stable_periods(DS)))
-    # for dur, gap in DURATION_MAX_GAP_PAIRS[0: -4]:
+    # print(len(get_full_keys_of_stable_periods(DatasetType.REDD)))
+    # print(len(get_full_keys_of_stable_periods(DatasetType.IDEAL)))
+
+    # for dur, gap in DURATION_MAX_GAP_PAIRS[22:25]:
     #     print(dur//DAY_IN_SEC, gap)
     #     process_stable_periods(DS, dur, gap)
-    #     print(len(get_full_keys_of_stable_periods(DS)))
+    # #     # print(len(get_full_keys_of_stable_periods(DS)))
 
 
     # full_keys = get_full_keys_of_stable_periods(DS)
