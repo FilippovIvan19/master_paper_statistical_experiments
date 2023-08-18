52cbd5fcb8371e62dba5900ea20d5c19f38a98a6
Filippov Ivan <fim1902@gmail.com>
2023-08-18 22:30:03 +0300

    power distance experiment

--- utils/constants.py
+++ utils/constants.py
@@ -18,6 +18,8 @@ CLEANED_PATH = '../data/cleaned_data_storage/'
 PERIODS_PATH = '../data/periods_storage/'
 POWER_PERIODS_PATH = '../data/power_periods_storage/'
 
+EXP_RESULTS_PATH = '../exp_results/'
+
 
 class DatasetType(enum.Enum):
     REDD = ('redd', convert_redd)
@@ -26,6 +28,13 @@ class DatasetType(enum.Enum):
     def __init__(self, path_str: str, nilmtk_converter: Callable):
         self.path_str: str = path_str
         self.nilmtk_converter: Callable = nilmtk_converter
+
+    @classmethod
+    def get_by_name(cls, name: str):
+        for elem in cls:
+            if elem.path_str == name:
+                return elem
+        return None
 
     def archive_path(self) -> str:
         return ARCHIVES_PATH + self.path_str
@@ -46,7 +55,7 @@ FULL_KEY_PATTERN = re.compile(r'/(\w+)/dur_(\w+)/gap_(\w+)/n_(\w+)')
 
 
 DURATION_MAX_GAP_PAIRS = [
-    (DAY_IN_SEC * 30, 300),
+    (DAY_IN_SEC * 30, 300),  # 0
     (DAY_IN_SEC * 20, 300),
     (DAY_IN_SEC * 15, 300),
     (DAY_IN_SEC * 10, 300),
@@ -60,7 +69,7 @@ DURATION_MAX_GAP_PAIRS = [
     (DAY_IN_SEC * 5, 90),
 
     (DAY_IN_SEC * 14, 60),
-    (DAY_IN_SEC * 10, 60),
+    (DAY_IN_SEC * 10, 60),  # 11
     (DAY_IN_SEC * 7, 60),
     (DAY_IN_SEC * 5, 60),
 
@@ -77,13 +86,13 @@ DURATION_MAX_GAP_PAIRS = [
     (DAY_IN_SEC * 3, 10),
     (DAY_IN_SEC, 10),
 
-    (DAY_IN_SEC * 5, 5),
+    (DAY_IN_SEC * 5, 5),  # 24
     (DAY_IN_SEC * 3, 5),
     (DAY_IN_SEC, 5),
 
     (DAY_IN_SEC * 5, 1),
     (DAY_IN_SEC * 3, 1),
-    (DAY_IN_SEC, 1),
+    (DAY_IN_SEC, 1),  # 29
 ]
 
 TIME_DELTAS = [
--- utils/experiment.py
+++ utils/experiment.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
-from utils.constants import DatasetType, DURATION_MAX_GAP_PAIRS
+from utils.constants import DatasetType, DURATION_MAX_GAP_PAIRS, EXP_RESULTS_PATH
 from utils.data_reading import get_full_keys_of_stable_periods, parse_full_key, \
     build_full_key, read_stable_period
-from utils.informative_index import accumulated_distance
+from utils.informative_index import accumulated_distance, power_distance
 from utils.parallelizer import parallelize
 from utils.preprocessing import generate_sync_signals, \
     generate_async_signals_by_point_count
@@ -39,4 +39,37 @@ def run_experiment_accum(ds: DatasetType, time_delta: int, out_file: str, part: 
     df = pd.DataFrame(data=parallelizer.storage,
                       columns=['ds', 'key', 'duration', 'max_gap', 'index', 'time_delta',
                                'point_count', 'resource_delta', 'sync_dist', 'async_dist'])
-    df.to_excel(out_file, sheet_name='accumulated_distance')
+    df.to_excel(EXP_RESULTS_PATH + out_file, sheet_name='accumulated_distance')
+
+
+def compute_power_dist_info(ds: DatasetType, full_key: str, time_delta: int) -> tuple:
+    with time_measure(f'compute_power_dist_info {ds.name}{full_key}'):
+        accumulated = read_stable_period(ds, full_key)
+        power = read_stable_period(ds, full_key, power_mode=True)
+
+        sync_signals = generate_sync_signals(accumulated, time_delta)
+        point_count = len(sync_signals)
+        resource_delta = accumulated.values[-1] / point_count
+        async_signals = generate_async_signals_by_point_count(accumulated, point_count)
+
+        sync_dist = power_distance(power, sync_signals)
+        async_dist = power_distance(power, async_signals)
+
+        key, duration, max_gap, index = parse_full_key(full_key)
+        info = (ds.name, key, duration, max_gap, index, time_delta, point_count,
+                resource_delta, sync_dist, async_dist)
+        return info
+
+
+def run_experiment_power(ds: DatasetType, time_delta: int, out_file: str, part: slice = slice(None), processes: int = 1) -> None:
+    with parallelize(processes, []) as parallelizer:
+        for (dur, gap) in DURATION_MAX_GAP_PAIRS[part]:
+            key_base = build_full_key('', dur, gap, '')
+            full_keys = get_full_keys_of_stable_periods(ds, key_base, power_mode=True)
+            for full_key in full_keys:
+                parallelizer.apply_async(compute_power_dist_info, (ds, full_key, time_delta))
+
+    df = pd.DataFrame(data=parallelizer.storage,
+                      columns=['ds', 'key', 'duration', 'max_gap', 'index', 'time_delta',
+                               'point_count', 'resource_delta', 'sync_dist', 'async_dist'])
+    df.to_excel(EXP_RESULTS_PATH + out_file, sheet_name='power_distance')
