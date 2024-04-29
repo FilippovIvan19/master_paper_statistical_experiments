b134c19826769afcefec148ba9c19ca22916fd20
Filippov Ivan <fim1902@gmail.com>
2024-04-29 16:01:17 +0300

    refactor and some helpful comments

--- main.py
+++ main.py
@@ -1,35 +1,17 @@
 import argparse
 
-import numpy as np
-from pandas import HDFStore
-
 from nilmtk import STATS_CACHE
 
-from utils.constants import DatasetType, DAY_IN_SEC, DURATION_MAX_GAP_PAIRS, TIME_DELTAS
-# from utils.data_reading import process_stable_periods
-# from utils.data_reading import clean_and_store_data, read_cleaned_data, \
-#     convert_to_nilmtk_format, store_processed_stable_periods, \
-#     get_full_keys_of_stable_periods, read_stable_periods, process_stable_periods, \
-#     build_full_key, parse_full_key
+from utils.constants import DatasetType, DURATION_MAX_GAP_PAIRS, TIME_DELTAS
 from utils.experiment import run_experiment_accum, run_experiment_power
-# from utils.informative_index import accumulated_distance, power_distance
-# from utils.preprocessing import generate_sync_signals, get_stable_periods, \
-#     interpolate_missed_data, generate_async_signals, reformat_to_accumulated, \
-#     generate_sync_signals_by_point_count, generate_async_signals_by_point_count
 from utils.timing import time_measure
-# from utils.visualization import plot_sync_async_comparison
 
-STATS_CACHE.store.close()
+STATS_CACHE.store.close()  # to avoid warning about non-closed file
 
 
-DS = DatasetType.IDEAL
-# DURATION = DAY_IN_SEC*30
-# MAX_GAP = 300
-
-# convert_to_nilmtk_format(DS)
-# clean_and_store_data(DS)
-# ideal = read_cleaned_data(DS)
-# process_stable_periods(DS, DURATION, MAX_GAP)
+# launch examples:
+# python main.py power ideal 7 11 12 4
+# python main.py accum redd 0 20 30
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -41,7 +23,7 @@ if __name__ == '__main__':
     parser.add_argument('processes', type=int, nargs='?', default=1)
     args = parser.parse_args()
 
-    by_resource_delta = True
+    by_resource_delta = True  # use this param to change limitation
     if args.experiment == 'accum':
         experiment_func = run_experiment_accum
         experiment_name = 'run_experiment_accum'
@@ -58,41 +40,3 @@ if __name__ == '__main__':
             args.processes,
             by_resource_delta
         )
-
-
-
-    # print(DatasetType.REDD)
-    # print(DatasetType.get_by_name('redd'))
-    # print(DatasetType.get_by_name('ideal'))
-    # print(DatasetType.get_by_name('hjkdfshjshjk'))
-
-
-    # dur, gap = DURATION_MAX_GAP_PAIRS[-1]
-    # print(dur//DAY_IN_SEC, gap)
-    # process_stable_periods(DS, dur, gap)
-
-
-    # print(len(get_full_keys_of_stable_periods(DatasetType.REDD)))
-    # print(len(get_full_keys_of_stable_periods(DatasetType.IDEAL)))
-
-    # for dur, gap in DURATION_MAX_GAP_PAIRS[22:25]:
-    #     print(dur//DAY_IN_SEC, gap)
-    #     process_stable_periods(DS, dur, gap)
-    # #     # print(len(get_full_keys_of_stable_periods(DS)))
-
-
-    # full_keys = get_full_keys_of_stable_periods(DS)
-    # accumulated = read_stable_periods(DS, [full_keys[0]])[0]
-    # # power = read_stable_periods(DS, [full_keys[0]], power_mode=True)[0]
-    #
-    # time_delta = 100
-    # sync_signals = generate_sync_signals(accumulated, time_delta)
-    # point_count = len(sync_signals)
-    # # print('len(sync_signals)', len(sync_signals))
-    #
-    # async_signals = generate_async_signals_by_point_count(accumulated, point_count)
-    # # print('len(async_signals)', len(async_signals))
-    #
-    # # plot_sync_async_comparison(accumulated, sync_signals, async_signals)
-    # accumulated_distance(accumulated, async_signals, 4)
-    # # power_distance(power, sync_signals, 4)
---
+++ periods_preprocessing_flow.py
@@ -0,0 +1,21 @@
+from utils.constants import DatasetType, DAY_IN_SEC
+from utils.data_reading import clean_and_store_data, convert_to_nilmtk_format, \
+    process_stable_periods
+
+
+# this file purpose is to demonstrate data preparation flow
+
+# setting params for data preprocessing
+DS = DatasetType.IDEAL
+DURATION = DAY_IN_SEC*30
+MAX_GAP = 300
+
+# was used only for IDEAL because REDD was gotten in already converted format
+convert_to_nilmtk_format(DS)
+
+# was used to delete nulls and incorrect (negative and infinite) values
+clean_and_store_data(DS)
+
+# was used to get stable periods, interpolate gaps and store
+# data in power and accumulated formats
+process_stable_periods(DS, DURATION, MAX_GAP)
